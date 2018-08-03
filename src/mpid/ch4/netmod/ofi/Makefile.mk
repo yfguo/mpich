65279110b8aa7d34a69b4430f8a27dc5a5b8b2b9
Yanfei Guo <yguo@anl.gov>
2018-08-03 00:02:59 -0500

    ch4/ofi: fix makefile

--- src/mpid/ch4/netmod/ofi/Makefile.mk
+++ src/mpid/ch4/netmod/ofi/Makefile.mk
@@ -16,6 +16,8 @@ noinst_HEADERS     +=
 mpi_core_sources   += src/mpid/ch4/netmod/ofi/func_table.c \
                       src/mpid/ch4/netmod/ofi/globals.c \
                       src/mpid/ch4/netmod/ofi/ofi_comm.c \
+                      src/mpid/ch4/netmod/ofi/ofi_spawn.c \
+                      src/mpid/ch4/netmod/ofi/ofi_win.c \
                       src/mpid/ch4/netmod/ofi/util.c
 
 errnames_txt_files += src/mpid/ch4/netmod/ofi/errnames.txt
