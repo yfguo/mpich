11fa1377db1c1d6b6d2752ac9dd49482a5d47672
Yanfei Guo <yguo@anl.gov>
2020-07-20 03:27:41 -0500

    ch4/ofi: implement PIPELINE send path
    This commit adds the PIPELINE AM send for OFI. OFI will first check the
    datatype and buffer location to determine if packing is needed.
    
    If packing is needed, a pack buffer will be created which matches. A chunk
    of the user buffer will be packed into it. The combined size of the
    actual packed data, OFI am header, CH4 am_hdr will be smaller than the
    max short message size of OFI. If the packing is not needed, OFI simply
    send from the user buffer chunk by chunk. The progress is stored in the
    parent send request's data_sz_left and offset. These parameters will be
    used to determine the src location for every send or pack.
    
    After sending the first chunk, the OFI enqueues the rest operation as a
    deferred isend and OFI progress will be responsible for issuing the
    result operation.
    
    If the send needs a pack buffer, but there is no free pack buffer
    available, the operation will be deferred. If the deferred queue is not
    empty, any new pipeline send operation is also deferred to maintain
    order.
    
    The progress engine will try issuing one chunk at a time if a pack
    buffer can be allocated. For OFI, each chunk need to have a separate
    request to track the on-the-fly send operation (this is because they
    need to have different fi_context which is embedded inside the OFI
    request object). Therefore, we may need to allocated child requests for
    the issuing of deferred chunks. However, if the chunk being issued is
    the first, we will use the original send request (parent request) rather
    than creating a child request. This was due to the cases in the previous
    paragraph. The management of child request and parent request utilizes
    the parent_req field introduced in the CH4 level.
    
    One thing to note is that the MPIDI_OFI_do_am_isend still has the
    capability of sending long message. This is because the AM SSEND and AM
    RMA still relies on this functionality. We need to address this problem
    later. An assert is added to make sure regular AM SEND does not
    accidentally try to send long message through it.

--- src/mpid/ch4/netmod/ofi/ofi_am_impl.c
+++ src/mpid/ch4/netmod/ofi/ofi_am_impl.c
@@ -8,6 +8,8 @@
 #include "ofi_am_impl.h"
 #include "ofi_noinline.h"
 
+static int issue_deferred_am_isend_eager(MPIDI_OFI_deferred_am_isend_req_t * dreq);
+static int issue_deferred_am_isend_pipeline(MPIDI_OFI_deferred_am_isend_req_t * dreq);
 
 void MPIDI_OFI_deferred_am_isend_dequeue(MPIDI_OFI_deferred_am_isend_req_t * dreq)
 {
@@ -22,7 +24,7 @@ void MPIDI_OFI_deferred_am_isend_dequeue(MPIDI_OFI_deferred_am_isend_req_t * dre
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_DEQUEUE);
 }
 
-int MPIDI_OFI_deferred_am_isend_issue(MPIDI_OFI_deferred_am_isend_req_t * dreq)
+static int issue_deferred_am_isend_eager(MPIDI_OFI_deferred_am_isend_req_t * dreq)
 {
     int mpi_errno = MPI_SUCCESS;
     int dt_contig = 0;
@@ -33,8 +35,8 @@ int MPIDI_OFI_deferred_am_isend_issue(MPIDI_OFI_deferred_am_isend_req_t * dreq)
     bool need_packing = false;
     bool data_lt_eager_threshold = false;
 
-    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_ISSUE);
-    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_ISSUE);
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_EAGER);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_EAGER);
 
     MPIDI_Datatype_get_info(dreq->count, dreq->datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
     need_packing = dt_contig ? false : true;
@@ -89,6 +91,137 @@ int MPIDI_OFI_deferred_am_isend_issue(MPIDI_OFI_deferred_am_isend_req_t * dreq)
     MPIR_ERR_CHECK(mpi_errno);
 
     MPIDI_OFI_deferred_am_isend_dequeue(dreq);
+
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_EAGER);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+static int issue_deferred_am_isend_pipeline(MPIDI_OFI_deferred_am_isend_req_t * dreq)
+{
+    int mpi_errno = MPI_SUCCESS;
+    int c = 0;
+    int dt_contig = 0;
+    char *send_buf;
+    MPI_Aint data_sz, packed_size;
+    MPI_Aint dt_true_lb, send_size;
+    MPIR_Datatype *dt_ptr;
+    bool data_lt_eager_threshold = false;
+    bool need_packing = false;
+    MPIR_Request *child_sreq = NULL;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_PIPELINE);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_PIPELINE);
+
+    MPIDI_Datatype_get_info(dreq->count, dreq->datatype, dt_contig, data_sz, dt_ptr, dt_true_lb);
+    send_size = MPIDI_OFI_DEFAULT_SHORT_SEND_SIZE - MPIDI_OFI_AMREQUEST_HDR(dreq->sreq, am_hdr_sz)
+        - sizeof(MPIDI_OFI_am_header_t);
+    need_packing = dt_contig ? false : true;
+
+    MPL_pointer_attr_t attr;
+    MPIR_GPU_query_pointer_attr(dreq->buf, &attr);
+    if (attr.type == MPL_GPU_POINTER_DEV && !MPIDI_OFI_ENABLE_HMEM) {
+        /* Force packing of GPU buffer in host memory */
+        need_packing = true;
+    }
+
+    /* check if we can get a pack buffer if needed. This is to avoid create a child request if
+     * no pack buffer can be allocated. */
+    if (need_packing) {
+        mpi_errno = MPIDU_genq_private_pool_alloc_cell(MPIDI_OFI_global.am_pack_buf_pool,
+                                                       (void **) &send_buf);
+        if (send_buf == NULL) {
+            /* no buffer available, suppress error and exit */
+            mpi_errno = MPI_SUCCESS;
+            goto fn_exit;
+        }
+        MPIR_ERR_CHECK(mpi_errno);
+    }
+
+    if (MPIDIG_REQUEST(dreq->sreq, req->lreq).offset == 0) {
+        child_sreq = dreq->sreq;
+    } else {
+        MPIR_cc_incr(dreq->sreq->cc_ptr, &c);
+        /* This child request is invisible to user, refcount should set to 1 */
+        child_sreq = MPIDIG_request_create(MPIR_REQUEST_KIND__SEND, 1);
+        MPIR_ERR_CHKANDSTMT((child_sreq) == NULL, mpi_errno, MPIX_ERR_NOREQ, goto fn_fail,
+                            "**nomemreq");
+        /* printf("child req %p\n", child_sreq); */
+
+        MPIDIG_REQUEST(child_sreq, req->lreq).parent_req = dreq->sreq;
+
+        MPIDIG_REQUEST(child_sreq, req->lreq).src_buf = dreq->buf;
+        MPIDIG_REQUEST(child_sreq, req->lreq).count = dreq->count;
+
+        MPIR_Datatype_add_ref_if_not_builtin(dreq->datatype);
+        MPIDIG_REQUEST(child_sreq, req->lreq).datatype = dreq->datatype;
+
+        MPIDIG_REQUEST(child_sreq, req->lreq).tag = MPIDIG_REQUEST(dreq->sreq, req->lreq).tag;
+        MPIDIG_REQUEST(child_sreq, req->lreq).rank = MPIDIG_REQUEST(dreq->sreq, req->lreq).rank;
+        MPIDIG_REQUEST(child_sreq, req->lreq).context_id =
+            MPIDIG_REQUEST(dreq->sreq, req->lreq).context_id;
+        MPIDIG_REQUEST(child_sreq, rank) = MPIDIG_REQUEST(dreq->sreq, rank);
+
+        mpi_errno = MPIDI_OFI_am_init_request(MPIDI_OFI_AMREQUEST_HDR(dreq->sreq, am_hdr),
+                                              MPIDI_OFI_AMREQUEST_HDR(dreq->sreq, am_hdr_sz),
+                                              child_sreq);
+        MPIR_ERR_CHECK(mpi_errno);
+
+    }
+
+    send_size = MPL_MIN(MPIDIG_REQUEST(dreq->sreq, req->lreq).data_sz_left, send_size);
+
+    if (need_packing) {
+        mpi_errno = MPIR_Typerep_pack(dreq->buf, dreq->count, dreq->datatype,
+                                      MPIDIG_REQUEST(dreq->sreq, req->lreq).offset, send_buf,
+                                      send_size, &packed_size);
+        MPIR_ERR_CHECK(mpi_errno);
+        send_size = packed_size;
+
+        MPIDI_OFI_AMREQUEST_HDR(child_sreq, pack_buffer) = send_buf;
+    } else {
+        send_buf = (char *) (dreq->buf) + dt_true_lb + MPIDIG_REQUEST(dreq->sreq, req->lreq).offset;
+        MPIDI_OFI_AMREQUEST_HDR(child_sreq, pack_buffer) = NULL;
+    }
+
+    mpi_errno = MPIDI_OFI_am_isend_short(dreq->rank, dreq->comm, dreq->handler_id, send_buf,
+                                         send_size, child_sreq);
+    MPIR_ERR_CHECK(mpi_errno);
+
+    MPIDIG_REQUEST(dreq->sreq, req->lreq).data_sz_left -= send_size;
+    MPIDIG_REQUEST(dreq->sreq, req->lreq).offset += send_size;
+
+    if (MPIDIG_REQUEST(dreq->sreq, req->lreq).data_sz_left == 0) {
+        /* only dequeue when all the data is sent */
+        MPIDI_OFI_deferred_am_isend_dequeue(dreq);
+    }
+
+  fn_exit:
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_ISSUE_DEFERRED_AM_ISEND_PIPELINE);
+    return mpi_errno;
+  fn_fail:
+    goto fn_exit;
+}
+
+int MPIDI_OFI_deferred_am_isend_issue(MPIDI_OFI_deferred_am_isend_req_t * dreq)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_ISSUE);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_ISSUE);
+
+    switch (dreq->op) {
+        case MPIDI_OFI_DEFERRED_AM_ISEND_OP__EAGER:
+            mpi_errno = issue_deferred_am_isend_eager(dreq);
+            break;
+        case MPIDI_OFI_DEFERRED_AM_ISEND_OP__PIPELINE:
+            mpi_errno = issue_deferred_am_isend_pipeline(dreq);
+            break;
+        default:
+            MPIR_Assert(0);
+    }
 
   fn_exit:
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_OFI_DEFERRED_AM_ISEND_ISSUE);
