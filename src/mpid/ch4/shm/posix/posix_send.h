717f48b6e8728124a5976ab8e127f888e73eb80d
Yanfei Guo <yguo@anl.gov>
2020-07-31 00:49:42 -0500

    ch4/shm: using new AM pt2pt interface
    Check datatype info and buffer attribute. Using these info to determine
    which AM protocol to be used.

--- src/mpid/ch4/shm/posix/posix_send.h
+++ src/mpid/ch4/shm/posix/posix_send.h
@@ -16,12 +16,22 @@
 
 #include "posix_impl.h"
 
+MPL_STATIC_INLINE_PREFIX size_t MPIDI_POSIX_am_eager_limit(void);
+
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_send(const void *buf, MPI_Aint count,
                                                   MPI_Datatype datatype, int rank, int tag,
                                                   MPIR_Comm * comm, int context_offset,
                                                   MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_send(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_send_new(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
+                               protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_send_coll(const void *buf, MPI_Aint count,
@@ -31,8 +41,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_send_coll(const void *buf, MPI_Aint cou
                                                    MPIR_Request ** request,
                                                    MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_send_coll(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
-                            errflag);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_send_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_ssend(const void *buf, MPI_Aint count,
@@ -40,7 +57,16 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_ssend(const void *buf, MPI_Aint cou
                                                    MPIR_Comm * comm, int context_offset,
                                                    MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_ssend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit
+        (data_sz, MPIDIG_SSEND_REQ, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_ssend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_isend(const void *buf, MPI_Aint count,
@@ -48,7 +74,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_isend(const void *buf, MPI_Aint cou
                                                    MPIR_Comm * comm, int context_offset,
                                                    MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_isend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_isend_coll(const void *buf, MPI_Aint count,
@@ -58,8 +92,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_isend_coll(const void *buf, MPI_Aint co
                                                     MPIR_Request ** request,
                                                     MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_isend_coll(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
-                             errflag);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_isend_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_issend(const void *buf, MPI_Aint count,
@@ -68,7 +109,16 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_issend(const void *buf, MPI_Aint co
                                                     MPIDI_av_entry_t * addr,
                                                     MPIR_Request ** request)
 {
-    return MPIDIG_mpi_issend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit
+        (data_sz, MPIDIG_SSEND_REQ, MPIDI_POSIX_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_issend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_cancel_send(MPIR_Request * sreq)
--- src/mpid/ch4/shm/src/shm_am_fallback_send.h
+++ src/mpid/ch4/shm/src/shm_am_fallback_send.h
@@ -14,7 +14,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_send(const void *buf,
                                                 MPIR_Comm * comm, int context_offset,
                                                 MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_send(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_send_new(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
+                               protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_ssend(const void *buf,
@@ -25,7 +33,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_ssend(const void *buf,
                                                  MPIR_Comm * comm, int context_offset,
                                                  MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_ssend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SSEND_REQ, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_ssend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_isend(const void *buf,
@@ -36,7 +52,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_isend(const void *buf,
                                                  MPIR_Comm * comm, int context_offset,
                                                  MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_isend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_issend(const void *buf,
@@ -47,7 +71,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_issend(const void *buf,
                                                   MPIR_Comm * comm, int context_offset,
                                                   MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_issend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SSEND_REQ, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_mpi_issend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_send_coll(const void *buf, MPI_Aint count,
@@ -56,8 +88,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_send_coll(const void *buf, MPI_Aint count
                                                  int context_offset, MPIDI_av_entry_t * addr,
                                                  MPIR_Request ** request, MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_send_coll(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
-                            errflag);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_send_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_isend_coll(const void *buf, MPI_Aint count,
@@ -66,8 +105,15 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_isend_coll(const void *buf, MPI_Aint coun
                                                   MPIDI_av_entry_t * addr, MPIR_Request ** request,
                                                   MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_isend_coll(buf, count, datatype, rank, tag, comm, context_offset, addr,
-                             request, errflag);
+    size_t data_sz;
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIDI_Datatype_check_size(count, datatype, data_sz);
+    if (!MPIDIG_am_check_size_le_eager_limit(data_sz, MPIDIG_SEND, MPIDI_SHM_am_eager_limit())) {
+        protocol = MPIDIG_AM_PROTOCOL__PIPELINE;
+    }
+    return MPIDIG_isend_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_mpi_cancel_send(MPIR_Request * sreq)
