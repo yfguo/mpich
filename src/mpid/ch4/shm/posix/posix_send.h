ea21425b7bd8847d0d4e89043679cd52e0dcd051
Yanfei Guo <yguo@anl.gov>
2020-07-31 00:49:42 -0500

    ch4/shm: using new AM pt2pt interface
    Check datatype info and buffer attribute. Using these info to determine
    which AM protocol to be used.

--- src/mpid/ch4/shm/posix/posix_send.h
+++ src/mpid/ch4/shm/posix/posix_send.h
@@ -21,7 +21,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_send(const void *buf, MPI_Aint coun
                                                   MPIR_Comm * comm, int context_offset,
                                                   MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_send(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SEND);
+    return MPIDIG_mpi_send_new(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
+                               protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_send_coll(const void *buf, MPI_Aint count,
@@ -31,8 +35,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_send_coll(const void *buf, MPI_Aint cou
                                                    MPIR_Request ** request,
                                                    MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_send_coll(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
-                            errflag);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SEND);
+    return MPIDIG_send_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_ssend(const void *buf, MPI_Aint count,
@@ -40,7 +47,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_ssend(const void *buf, MPI_Aint cou
                                                    MPIR_Comm * comm, int context_offset,
                                                    MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_ssend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SSEND_REQ);
+    return MPIDIG_mpi_ssend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_isend(const void *buf, MPI_Aint count,
@@ -48,7 +59,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_isend(const void *buf, MPI_Aint cou
                                                    MPIR_Comm * comm, int context_offset,
                                                    MPIDI_av_entry_t * addr, MPIR_Request ** request)
 {
-    return MPIDIG_mpi_isend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SEND);
+    return MPIDIG_mpi_isend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_isend_coll(const void *buf, MPI_Aint count,
@@ -58,8 +73,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_isend_coll(const void *buf, MPI_Aint co
                                                     MPIR_Request ** request,
                                                     MPIR_Errflag_t * errflag)
 {
-    return MPIDIG_isend_coll(buf, count, datatype, rank, tag, comm, context_offset, addr, request,
-                             errflag);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SEND);
+    return MPIDIG_isend_coll_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, errflag, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_issend(const void *buf, MPI_Aint count,
@@ -68,7 +86,11 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_issend(const void *buf, MPI_Aint co
                                                     MPIDI_av_entry_t * addr,
                                                     MPIR_Request ** request)
 {
-    return MPIDIG_mpi_issend(buf, count, datatype, rank, tag, comm, context_offset, addr, request);
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    protocol = MPIDI_SHM_am_choose_protocol(buf, count, datatype, 0, MPIDIG_SSEND_REQ);
+    return MPIDIG_mpi_issend_new(buf, count, datatype, rank, tag, comm, context_offset, addr,
+                                 request, protocol);
 }
 
 MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_mpi_cancel_send(MPIR_Request * sreq)
