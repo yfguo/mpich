7ceb8b2ad0cdd4def77e791e4911985df2c1a926
Yanfei Guo <yguo@anl.gov>
2020-08-09 22:59:20 -0500

    ch4/shm: Add framework for new AM protocol.
    Add interface for sending PIPELINE RTS. This is for the netmod to
    initiate the PIPELINE protocol. It takes the parameters of the send
    operation and prepare the netmod level send for RTS metadata and the
    first segment of data.
    
    Add interface for sending PIPELINE SEG. This is for sending the rest
    data segment through the PIPELINE protocol.
    
    Add new interface for checking protocol for AM buffer and OP. We have to
    provide this because the AM RMA would run into the case where it need to
    determine the protocol on target side. For example, the ACK of a GET and
    the PUT where the target_addr's location can only be determined by the
    target process. But because it is an RMA operation, because the target
    process only have callbacks to handle the RMA, the regular checking
    design in posix_rma.h is insufficient---SHM must expose
    an interface for that.

--- src/mpid/ch4/shm/posix/posix_am.h
+++ src/mpid/ch4/shm/posix/posix_am.h
@@ -10,6 +10,7 @@
 #include "posix_am_impl.h"
 #include "posix_eager.h"
 #include "mpidu_genq.h"
+#include "shm.h"
 
 /* Enqueue a request header onto the postponed message queue. This is a helper function and most
  * likely shouldn't be used outside of this file. */
@@ -403,6 +404,58 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_am_send_hdr_reply(MPIR_Context_id_t con
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_AM_SEND_HDR_REPLY);
 
     return mpi_errno;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_am_isend_pipeline_rts(int rank, MPIR_Comm * comm,
+                                                               MPIDI_POSIX_am_header_kind_t kind,
+                                                               int handler_id, const void *am_hdr,
+                                                               size_t am_hdr_sz, const void *data,
+                                                               MPI_Count count,
+                                                               MPI_Datatype datatype,
+                                                               MPIR_Request * sreq)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_RTS);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_RTS);
+
+    MPIR_Assert(0);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_RTS);
+
+    return mpi_errno;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_am_isend_pipeline_seg(MPIR_Context_id_t context_id,
+                                                               int src_rank,
+                                                               MPIDI_POSIX_am_header_kind_t kind,
+                                                               int handler_id, const void *am_hdr,
+                                                               size_t am_hdr_sz, const void *data,
+                                                               MPI_Count count,
+                                                               MPI_Datatype datatype,
+                                                               MPIR_Request * sreq)
+{
+    int mpi_errno = MPI_SUCCESS;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_SEG);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_SEG);
+
+    MPIR_Assert(0);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_POSIX_AM_ISEND_PIPELINE_SEG);
+
+    return mpi_errno;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_POSIX_am_choose_protocol(const void *buf, MPI_Count count,
+                                                            MPI_Datatype datatype, size_t am_ext_sz,
+                                                            int handler_id)
+{
+    int protocol = MPIDIG_AM_PROTOCOL__EAGER;
+
+    MPIR_Assert(0);
+
+    return protocol;
 }
 
 #endif /* POSIX_AM_H_INCLUDED */
