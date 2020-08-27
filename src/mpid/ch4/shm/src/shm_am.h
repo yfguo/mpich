ca5fbf4519d5a2e1f1144fda341a4b0c0be3f474
Yanfei Guo <yguo@anl.gov>
2020-08-27 17:30:22 -0500

    ch4/shm: Add framework for new AM protocol.
    Add interface for sending PIPELINE RTS. This is for the netmod to
    initiate the PIPELINE protocol. It takes the parameters of the send
    operation and prepare the netmod level send for RTS metadata and the
    first segment of data.
    
    Add interface for sending PIPELINE SEG. This is for sending the rest
    data segment through the PIPELINE protocol.

--- src/mpid/ch4/shm/src/shm_am.h
+++ src/mpid/ch4/shm/src/shm_am.h
@@ -94,6 +94,44 @@ MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_isend_reply(MPIR_Context_id_t context_
     return ret;
 }
 
+MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_isend_pipeline_rts(int rank, MPIR_Comm * comm,
+                                                             int handler_id, const void *am_hdr,
+                                                             size_t am_hdr_sz, const void *data,
+                                                             MPI_Count count, MPI_Datatype datatype,
+                                                             MPIR_Request * sreq)
+{
+    int ret;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_RTS);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_RTS);
+
+    ret = MPIDI_POSIX_am_isend_pipeline_rts(rank, comm, MPIDI_POSIX_AM_HDR_CH4, handler_id, am_hdr,
+                                            am_hdr_sz, data, count, datatype, sreq);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_RTS);
+    return ret;
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_isend_pipeline_seg(MPIR_Context_id_t context_id,
+                                                             int src_rank, int handler_id,
+                                                             const void *am_hdr, size_t am_hdr_sz,
+                                                             const void *data, MPI_Count count,
+                                                             MPI_Datatype datatype,
+                                                             MPIR_Request * sreq)
+{
+    int ret;
+
+    MPIR_FUNC_VERBOSE_STATE_DECL(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_SEG);
+    MPIR_FUNC_VERBOSE_ENTER(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_SEG);
+
+    ret = MPIDI_POSIX_am_isend_pipeline_seg(context_id, src_rank, MPIDI_POSIX_AM_HDR_CH4,
+                                            handler_id, am_hdr, am_hdr_sz, data, count, datatype,
+                                            sreq);
+
+    MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_SHM_AM_ISEND_PIPELINE_SEG);
+    return ret;
+}
+
 MPL_STATIC_INLINE_PREFIX size_t MPIDI_SHM_am_hdr_max_sz(void)
 {
     int ret;
@@ -136,6 +174,13 @@ MPL_STATIC_INLINE_PREFIX void MPIDI_SHM_am_request_finalize(MPIR_Request * req)
     MPIDI_POSIX_am_request_finalize(req);
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_SHM_AM_REQUEST_FINALIZE);
+}
+
+MPL_STATIC_INLINE_PREFIX int MPIDI_SHM_am_choose_protocol(const void *buf, MPI_Count count,
+                                                          MPI_Datatype datatype, size_t am_ext_sz,
+                                                          int handler_id)
+{
+    return MPIDI_POSIX_am_choose_protocol(buf, count, datatype, am_ext_sz, handler_id);
 }
 
 #endif /* SHM_AM_H_INCLUDED */
