43d890170e39a9a7c3722b60a8ded5b7fb908732
Yanfei Guo <yguo@anl.gov>
2021-07-01 11:41:11 -0500

    mpl/gpu: add wrapper for launch host function on GPU stream

--- src/mpl/include/mpl_gpu_fallback.h
+++ src/mpl/include/mpl_gpu_fallback.h
@@ -9,6 +9,8 @@
 typedef int MPL_gpu_ipc_mem_handle_t;
 typedef int MPL_gpu_device_handle_t;
 typedef int MPL_gpu_device_attr;        /* dummy type */
+typedef void *MPL_gpu_stream_t; /* dummy type */
+typedef void *MPL_gpu_host_fn_t;        /* dummy type */
 #define MPL_GPU_DEVICE_INVALID -1
 
 #endif /* ifndef MPL_GPU_CUDA_H_INCLUDED */
