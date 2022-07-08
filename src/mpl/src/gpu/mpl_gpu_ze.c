4763979274e42bb7db7bc0fe2be9e8f85f7c6c63
Sudheer Chunduri <sudheer@anl.gov>
2022-07-08 02:32:32 +0000

    hydra: include support for gpu subdevice binding options
    loop through the subdevices on the gpu coprocessors
    Intel ze specific options are included

--- src/mpl/src/gpu/mpl_gpu_ze.c
+++ src/mpl/src/gpu/mpl_gpu_ze.c
@@ -44,23 +44,23 @@ int MPL_gpu_get_dev_count(int *dev_cnt, int *dev_id)
     return ret;
 }
 
-int MPL_gpu_get_dev_list(int *dev_list)
+int MPL_gpu_get_dev_list(int **dev_list)
 {
     int ret = MPL_SUCCESS;
     *dev_list = NULL;
     return ret;
 }
 
-int MPL_gpu_get_subdev_list(int dev_id, int *subdev_list)
+int MPL_gpu_get_subdev_list(int dev_id, int **subdev_list)
 {
-    int ret = MPI_SUCCESS;
+    int ret = MPL_SUCCESS;
     *subdev_list = NULL;
     return ret;
 }
 
 int MPL_gpu_dev_affinity_to_env(int dev_count, int *dev_ids, char **env)
 {
-    int ret = MPI_SUCCESS;
+    int ret = MPL_SUCCESS;
     *env = NULL;
     return ret;
 }
