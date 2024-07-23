e18c16d6bdfd43a42c0563af43574a77183ba14c
Yanfei Guo <yguo@anl.gov>
2024-07-22 22:16:48 -0500

    configure: consolidate and update instruction set related options
    1. Consolidates all the AVX and SSE instruction related options into
    MPICH configure. The CFLAGS setting has to be in MPICH configure to work.
    Moving MPL's checking into MPICH makes it easier to maintain and track
    changes.
    2. Separate AVX512 options because it was breaking the build on AMD
    Zen3 as only AVX256 is supported.

---
+++ confdb/aclocal_arch.m4
@@ -0,0 +1,6 @@
+dnl Architecture related definitions for autoheader
+
+AH_TEMPLATE([HAVE_MM512_STOREU_SI512], [Define to 1 if _mm512_storeu_si512 is available])
+AH_TEMPLATE([HAVE_MM256_STOREU_SI256], [Define to 1 if _mm256_storeu_si256 is available])
+AH_TEMPLATE([HAVE_MM256_STREAM_SI256], [Define to 1 if _mm256_stream_si256 is available])
+AH_TEMPLATE([HAVE_MM_STREAM_SI128], [Define to 1 if _mm_stream_si128 is available])
--- src/mpl/include/mpl_trmem.h
+++ src/mpl/include/mpl_trmem.h
@@ -232,7 +232,7 @@ typedef struct {
 
 #include "string.h"
 
-#ifdef HAVE_MM256_STREAM_SI256
+#ifdef MPL_HAVE_MM256_STREAM_SI256
 #include <immintrin.h>
 
 static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
@@ -316,7 +316,7 @@ static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
 
 #else
 
-#ifdef HAVE_SSE2
+#ifdef MPL_HAVE_MM_STREAM_SI128
 #include <immintrin.h>
 
 static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
@@ -396,7 +396,7 @@ static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
     memcpy(dest, src, n);
 }
 
-#endif /* HAVE_SSE2 */
+#endif /* MPL_HAVE_MM_STREAM_SI128 */
 
 #endif
 
