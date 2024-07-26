6bb27fa7f90dc30dbab374751e5e5d0e8cd123fa
Yanfei Guo <yguo@anl.gov>
2024-07-25 22:24:33 -0500

    configure: cleanup arch specific fast options
    1. Separate arch  related fast options to `sse2`, `avx` and
    `avx512f`. The `sse2` is enabled by default because it is widely
    supported. The MPICH configure will check if the compiler supports
    the corresponding options and add them to CFLAGS.
    
    2. Consolidate individual intrinsic checks in MPL configure. Only
    MPL needs the these macro definitions.

--- src/mpl/include/mpl_trmem.h
+++ src/mpl/include/mpl_trmem.h
@@ -232,7 +232,7 @@ typedef struct {
 
 #include "string.h"
 
-#ifdef HAVE_MM256_STREAM_SI256
+#ifdef MPL_HAVE__MM256_STREAM_SI256
 #include <immintrin.h>
 
 static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
@@ -316,7 +316,7 @@ static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
 
 #else
 
-#ifdef HAVE_SSE2
+#ifdef MPL_HAVE__MM_STREAM_SI128
 #include <immintrin.h>
 
 static inline void MPL_Memcpy_stream(void *dest, const void *src, size_t n)
